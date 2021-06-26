1de5ab2edd7c959d53f3d4611d021eab229c3a17
jamie <jamie.xue@gmail.com>
2021-06-26 19:41:26 -0400

    mess up deck setup on dev for fun

--- poker_modules/deck.js
+++ poker_modules/deck.js
@@ -3,16 +3,17 @@
  */
 var Deck = function() {
 	this.nextCard = 0;
+  // let's hit some high hands, kings full of kings!
   this.cards = ['As', 'Ah', 'Ad', 'Ac',
                 'Ks', 'Kh', 'Kd', 'Kc',
-                'Qs', 'Qh', 'Qd', 'Qc',
-                'Js', 'Jh', 'Jd', 'Jc',
-                'Ts', 'Th', 'Td', 'Tc',
-                '9s', '9h', '9d', '9c',
-                '8s', '8h', '8d', '8c',
-                '7s', '7h', '7d', '7c',
-                '6s', '6h', '6d', '6c',
-                '5s', '5h', '5d', '5c',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
+                'Ks', 'Kh', 'Kd', 'Kc',
                 '4s', '4h', '4d', '4c',
                 '3s', '3h', '3d', '3c',
                 '2s', '2h', '2d', '2c'];
